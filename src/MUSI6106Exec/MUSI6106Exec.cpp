7cacb1c512380ad1017d2443eb1e26e81d0f4504
alexanderlerch <alexander.lerch@gatech.edu>
2022-03-02 14:15:55 -0500

    reference solution for exercise 3

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -3,9 +3,11 @@
 #include <ctime>
 
 #include "MUSI6106Config.h"
-#include "Fft.h"
-#include "RingBuffer.h"
+
 #include "AudioFileIf.h"
+#include "RingBuffer.h"
+#include "Fft.h"
+#include "Vector.h"
 
 using std::cout;
 using std::endl;
@@ -17,53 +19,65 @@ void    showClInfo();
 // main function
 int main(int argc, char* argv[])
 {
-    std::string             sInputFilePath,                 //!< file paths
+    std::string sInputFilePath,                 //!< file paths
         sOutputFilePath;
 
-    static const int        kBlockSize = 1024;
+    int iBlockLength = 4096,
+        iHopLength = 2048;
 
-    clock_t                 time = 0;
+    clock_t time = 0;
 
     float** ppfAudioData = 0;
-    int fftBlockSize = 0;
-    int fftHopSize = 0;
+
+    float* pfSpectrum = 0;
+
     CAudioFileIf* phAudioFile = 0;
-
-    float *ppfInputBuffer = nullptr;
-//    float *ppfMagnitudeBuffer = nullptr;
-    CFft* cFFT = nullptr;
-    CRingBuffer<float> *ringBuffer = nullptr;
-    CFft::complex_t *pfSpectrum=nullptr;
-    float *pfMagnitude = nullptr;
-    float *pfPhase = nullptr;
-
-    std::fstream            hOutputFile;
+    std::fstream hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
+
+    CRingBuffer<float> *pCRingBuffer = 0;
+    CFft* pCFft = 0;
 
     showClInfo();
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-    if (argc < 3)
+    if (argc < 2)
     {
         cout << "Missing audio input path!";
         return -1;
     }
     else
     {
-        sInputFilePath = argv[1];
-        fftBlockSize = std::stoi(argv[2]);
-        fftHopSize = std::stoi(argv[3]);
-        sOutputFilePath = sInputFilePath.substr(0,sInputFilePath.size()-4) + ".txt";
+        if (argc > 3)
+        {
+            iHopLength = std::stoi(argv[3]);
+        }
+        if (argc > 2)
+        {
+            iBlockLength = std::stoi(argv[2]);
+        }
+        if (argc > 1)
+        {
+            sInputFilePath = argv[1];
+            sOutputFilePath = sInputFilePath + ".txt";
+        }
     }
+
+    //////////////////////////////////////////////////////////////////////////////
+    // initialize read buffer
+    pCRingBuffer = new CRingBuffer<float>(iBlockLength);
+    pCRingBuffer->setWriteIdx(iBlockLength - iHopLength);
+
+    //////////////////////////////////////////////////////////////////////////////
+    // initialize FFT and fft output buffer
+    CFft::createInstance(pCFft);
+    pCFft->initInstance(iBlockLength);
+    pfSpectrum = new float[iBlockLength];
 
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
     CAudioFileIf::create(phAudioFile);
-    CFft::createInstance(cFFT);
-    // INIT FFT
-    cFFT->initInstance(fftBlockSize,1,CFft::kWindowHann);
-
     phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
     if (!phAudioFile->isOpen())
     {
@@ -72,8 +86,7 @@ int main(int argc, char* argv[])
         return -1;
     }
     phAudioFile->getFileSpec(stFileSpec);
-    // FIXME: does ringBuffer need to be block+hop, or is just block enough?
-    ringBuffer = new CRingBuffer<float>(fftBlockSize+fftHopSize);
+
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
     hOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
@@ -86,10 +99,9 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
-
     ppfAudioData = new float* [stFileSpec.iNumChannels];
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        ppfAudioData[i] = new float[kBlockSize];
+        ppfAudioData[i] = new float[iBlockLength];
 
     if (ppfAudioData == 0)
     {
@@ -103,66 +115,67 @@ int main(int argc, char* argv[])
         hOutputFile.close();
         return -1;
     }
-    ppfInputBuffer = new float[kBlockSize];
 
-    pfSpectrum = new float[kBlockSize];
-    pfMagnitude = new float[(kBlockSize/2)+1];
-    pfPhase = new float[(kBlockSize/2)+1];
     time = clock();
-//    hOutputFile << "TEST TEST TEST";
+
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
     while (!phAudioFile->isEof())
     {
         // set block length variable
-        long long iNumFrames = kBlockSize;
+        long long iNumFrames = iHopLength;
 
         // read data (iNumOfFrames might be updated!)
         phAudioFile->readData(ppfAudioData, iNumFrames);
 
-        cout << "\r" << "reading and writing";
-
-        // write
-        for (int i = 0; i < iNumFrames; i++)
+        // set buffer to zero if not written (EOF)
+        if (iNumFrames < iHopLength)
         {
             for (int c = 0; c < stFileSpec.iNumChannels; c++)
-            {
-                ringBuffer->putPostInc(ppfAudioData[c][i]);
-                //FIXME: After the first iteration, the number of values in buffer will always be fftBlockSize, find a way to fix this and move ahead by hopSize
-                if (ringBuffer->getNumValuesInBuffer() >= fftBlockSize)
-                {
-                    // Copy data from ringBuffer to ppfInputBuffer (getpostinc?)
-                    for(int j=0;j<fftBlockSize;j++)
-                    {
-                        ppfInputBuffer[j] = ringBuffer->getPostInc();
-                    }
-                    // Run fft
-                    cFFT->doFft(pfSpectrum,ppfInputBuffer);
-                    cFFT->getMagnitude(pfMagnitude,pfSpectrum);
-                    cFFT->getPhase(pfPhase,pfSpectrum);
-
-                    // TODO: Write magnitude and phase to hOutputFile
-                    //std::cout<<"FFT Block done"<<std::endl;
-                    for(int k=0;k<((fftBlockSize/2)+1);k++)
-                        hOutputFile << pfMagnitude[k] << "," << pfPhase[k] << "\n";
-
-//                    for(int l=0;l<fftHopSize;l++)
-//                        ringBuffer->getPostInc();
-                }
-                //hOutputFile << ppfAudioData[c][i] << "\t";
-            }
-            hOutputFile << endl;
+                CVectorFloat::setZero(&ppfAudioData[c][iNumFrames], iHopLength - iNumFrames);
         }
+
+        // downmix in case of multichannel
+        for (int c = 1; c < stFileSpec.iNumChannels; c++)
+            CVectorFloat::add_I(ppfAudioData[0], ppfAudioData[c], iNumFrames);
+        CVectorFloat::mulC_I(ppfAudioData[0], 1.F / stFileSpec.iNumChannels, iNumFrames);
+
+        // write data into ringbuffer
+        pCRingBuffer->putPostInc(ppfAudioData[0], iHopLength);
+
+        cout << "\r" << "computing FFT and writing file";
+
+        // get data from ringbuffer and increment read index
+        pCRingBuffer->get(ppfAudioData[0], iBlockLength);
+        pCRingBuffer->setReadIdx(pCRingBuffer->getReadIdx() + iHopLength);
+
+        // compute magnitude spectrum (hack
+        pCFft->doFft(pfSpectrum, ppfAudioData[0]);
+        pCFft->getMagnitude(ppfAudioData[0], pfSpectrum);
+
+        // write
+        // note that this solution is, compared to the matlab reference, shifted by one block and scaled by 0.5
+        for (int i = 0; i < (iBlockLength>>1) + 1; i++)
+        {
+            hOutputFile << ppfAudioData[0][i] << "\t";
+        }
+        hOutputFile << endl;
     }
 
     cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
 
     //////////////////////////////////////////////////////////////////////////////
-    // clean-up (close files and free memory)
+    // clean-up (close files, delete instances, and free memory)
     CAudioFileIf::destroy(phAudioFile);
-    CFft::destroyInstance(cFFT);
     hOutputFile.close();
 
+    CFft::destroyInstance(pCFft);
+    pCFft = 0;
+    delete pCRingBuffer;
+    pCRingBuffer = 0;
+
+    delete[] pfSpectrum;
+    pfSpectrum = 0;
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
         delete[] ppfAudioData[i];
     delete[] ppfAudioData;
